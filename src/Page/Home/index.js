5621d55a699187aaf8ecb1c13914cc097568b38f
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2023-12-17 11:55:53 -0300

    update home

--- src/Page/Home/index.js
+++ src/Page/Home/index.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom"
 import NavBar from "../../Components/NavBar"
 import Search from "../../Components/Search"
+import Catalogo from "../../Components/Catalogo"
 function Home () { 
     return( 
         <div className="container'">
             <Search/>
             <NavBar/>
+            <Catalogo/> 
         </div>
 
     )
