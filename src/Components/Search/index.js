6a62eea5c4b3d83a4334f40c732cb385229ce81a
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2023-12-17 14:12:58 -0300

    update

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -3,15 +3,14 @@ import { Link } from "react-router-dom"
 function Search () {
     return(
         <div className="container-search">
-            <ul>
-                <li>Logo</li>
-                <li>
-                    <input type="search"/>
-                    <i class='bx bx-search'></i>
-                </li>
-                <li><i class='bx bx-cart-download'></i></li>
-                <li> <Link to={'/painelAdm'}> <i class='bx bxs-folder-open'></i> </Link></li>
-            </ul>
+            <div className="nav">
+                <div> <Link  className="logo" to={'/'} >JS.STORE</Link> </div>
+                <div className="nav-dados">
+                    <div><i class='bx bx-cart-download'></i></div>
+                    <div> <Link to={'/painelAdm'}> <i class='bx bxs-folder-open'></i> </Link></div>
+                    <div><i class='bx bxs-user' ></i></div>
+                </div>
+            </div>
         </div>
     )
 }
