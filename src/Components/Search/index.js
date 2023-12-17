314ec946794a4a5117bd27d577f9f5db4a4a07c9
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2023-12-17 13:40:36 -0300

    update routes

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,4 +1,5 @@
 import "./style.css"
+import { Link } from "react-router-dom"
 function Search () {
     return(
         <div className="container-search">
@@ -9,6 +10,7 @@ function Search () {
                     <i class='bx bx-search'></i>
                 </li>
                 <li><i class='bx bx-cart-download'></i></li>
+                <li> <Link to={'/painelAdm'}> <i class='bx bxs-folder-open'></i> </Link></li>
             </ul>
         </div>
     )
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom" 
 import Home from "./page/Home"
+import PainelAdm from "./page/PainelAdm"
 function RoutesApp () {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/painelAdm" element={<PainelAdm/>} />
             </Routes>
         </BrowserRouter>
     )
