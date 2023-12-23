6accf96ebb448ca955dcb40303c1abcb7e4a0d51
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2023-12-23 20:58:29 -0300

    s

---
+++ src/Page/Inicio/index.js
@@ -0,0 +1,14 @@
+import NavBar from '../../Components/NavBar'
+import Catalogo from '../../Components/Catalogo'
+import Destaque from '../../Components/Destaque'
+function Inicio() {
+  return (
+    <div className="container'">
+      <NavBar />
+      <Destaque />
+      <Catalogo />
+    </div>
+  )
+}
+
+export default Inicio
